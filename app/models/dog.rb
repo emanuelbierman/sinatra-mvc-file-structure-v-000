00b606ee5138d33b985cc49076b2dcc964c9f401
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-03 17:38:09 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -10,9 +10,9 @@ class Dog < Model
   @@all = []
 
   def initialize(name, breed, age)
-    self.name = :name
-    self.breed = :breed
-    self.age = :age
+    self.name = name
+    self.breed = breed
+    self.age = age
     @@all << self
   end
 
